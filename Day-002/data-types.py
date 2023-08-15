9f1579537466055a39a5d4da132417c6b472a5f9
smhamidi <1380hamidi@gmail.com>
2023-08-15 17:44:44 +0330

    day two started and the first challenge is done

---
+++ Day-002/data-types.py
@@ -0,0 +1,27 @@
+# Data Types
+
+# Strings
+
+print("Hello"[-1])
+
+print("123" + "234")
+
+print("Hello" + " Mohammad")
+
+# integer
+
+print(123 + 321)
+
+# you can put underscore in the middle of an integer
+100_000_000
+
+# float
+
+314.59
+
+# bool
+# it is either True or False
+
+num_char = len(input("What's your name ?"))
+new_num_char = str(num_char)
+print("Your Name Contains " + new_num_char + " characters")
---
+++ Day-002/first-challenge.py
@@ -0,0 +1,7 @@
+input_number = input("Please enter a number: \n")
+
+sum = 0
+for number in str(input_number):
+    sum += int(number)
+
+print(sum)
